61726e6df15ee22cba7fb82ae3a0ba26a71aa6c1
Pavel <p97p@ya.ru>
2020-06-13 01:38:31 +0300

    switch from moor_ffi to moor_flutter as fii is yet unstable

--- lib/data/db.dart
+++ lib/data/db.dart
@@ -1,11 +1,8 @@
 import 'package:employees/models/user.dart';
 import 'package:employees/utils.dart';
 import 'package:flutter/foundation.dart';
-import 'package:moor_ffi/moor_ffi.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
+import 'package:moor_flutter/moor_flutter.dart';
 import 'package:moor/moor.dart';
-import 'dart:io';
 
 part 'db.g.dart';
 
@@ -28,18 +25,9 @@ class Children extends Table {
   IntColumn get parentId => integer()();
 }
 
-
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(path.join(dbFolder.path, 'db.sqlite'));
-    return VmDatabase(file);
-  });
-}
-
 @UseMoor(tables: [Employees, Children])
 class MyDatabase extends _$MyDatabase {
-  MyDatabase() : super(_openConnection());
+  MyDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite'));
 
   @override
   int get schemaVersion => 1;
